780672c8c9202022aa8a00f5a6e00fab0c454346
Tofeeq Rehman <tofeeq3@gmail.com>
2017-03-02 06:13:14 +0000

    Update config_email.php

--- config_email.php
+++ config_email.php
@@ -1,12 +1,7 @@
 {
-
     "smtp_backend" 	: "phpmailer",
-
     "smtp_host"  	: "smtp.gmail.com",
     "smtp_port"  	: "465",
-
     "smtp_username" : "your smtp username",
     "smtp_password" : "your smtp password",
-
-    "instructions" 	: "set mode to smtp if using smtp, choose one from backend ie swiftmailer or phpmailer"
 }
